e606478ee53448a656034dfb875b5e9cf8760ba6
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-14 12:32:27 +0600

    add create post function

--- src/contexts/PostProvider.tsx
+++ src/contexts/PostProvider.tsx
@@ -1,6 +1,7 @@
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { Comment, Like, Post } from "@/types";
 import PostContext from "./PostContext";
+import { useEffect } from "react";
 
 export default function PostProvider({
 	children,
@@ -11,9 +12,23 @@ export default function PostProvider({
 		[]
 	);
 	const [likes, setLikes] = useLocalStorage<Like[] | []>("likes", []);
+
+	function createPost(authorId: number, content: string, title: string = "") {
+		const id = posts.findLast(post => post.id)?.id || 0;
+		const newPost: Post = {
+			id: id + 1,
+			authorId: authorId,
+			title: title,
+			content: content,
+			createdAt: new Date().toUTCString(),
+			updatedAt: new Date().toUTCString(),
+		};
+		setPosts(prev => [newPost, ...prev]);
+	}
+
 	return (
 		<PostContext.Provider
-			value={{ posts, setPosts, comments, setComments, likes, setLikes }}
+			value={{ posts, setPosts, comments, setComments, likes, setLikes, createPost }}
 		>
 			{children}
 		</PostContext.Provider>
