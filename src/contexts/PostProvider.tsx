22e3edde26e53be3a46ab73fa0ef12fd5a952c14
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-15 14:54:10 +0600

    add post like toggle function and like count funtion

--- src/contexts/PostProvider.tsx
+++ src/contexts/PostProvider.tsx
@@ -1,7 +1,6 @@
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { Comment, Like, Post } from "@/types";
 import PostContext from "./PostContext";
-import { useEffect } from "react";
 
 export default function PostProvider({
 	children,
@@ -14,7 +13,7 @@ export default function PostProvider({
 	const [likes, setLikes] = useLocalStorage<Like[] | []>("likes", []);
 
 	function createPost(authorId: number, content: string, title: string = "") {
-		const id = posts.findLast(post => post.id)?.id || 0;
+		const id = posts.findLast((post) => post.id)?.id || 0;
 		const newPost: Post = {
 			id: id + 1,
 			authorId: authorId,
@@ -23,12 +22,51 @@ export default function PostProvider({
 			createdAt: new Date().toUTCString(),
 			updatedAt: new Date().toUTCString(),
 		};
-		setPosts(prev => [newPost, ...prev]);
+		setPosts((prev) => [newPost, ...prev]);
+	}
+
+	function isPostLikedByAuthor(postId: number, authorId: number): Like | undefined {
+		return likes.find(
+			(like) => like.postId === postId && like.authorId === authorId
+		);
+	}
+
+	function toggleLike(postId: number, authorId: number) {
+		setLikes(prev => {
+			const isExists = prev.find((like) => like.postId === postId && like.authorId === authorId);
+			if (isExists) {
+				return [...prev].filter(like => like.id !== isExists.id);
+			}
+			const id = prev.findLast((like) => like.id)?.id || 0;
+			const newLike: Like = {
+				id: id + 1,
+				postId: postId,
+				authorId: authorId,
+				createdAt: new Date().toUTCString(),
+				updatedAt: new Date().toUTCString(),
+			};
+			return [...prev, newLike];
+		})
+	}
+
+	function getTotalLikeByPostId(postId: number): number {
+		return likes.filter((like) => like.postId === postId).length;
 	}
 
 	return (
 		<PostContext.Provider
-			value={{ posts, setPosts, comments, setComments, likes, setLikes, createPost }}
+			value={{
+				posts,
+				setPosts,
+				comments,
+				setComments,
+				likes,
+				setLikes,
+				createPost,
+				toggleLike,
+				getTotalLikeByPostId,
+				isPostLikedByAuthor,
+			}}
 		>
 			{children}
 		</PostContext.Provider>
