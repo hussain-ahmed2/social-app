71d349dc895a0b8d5f855f9fe9c3006b4d4f769d
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-16 21:38:57 +0600

    update css

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import AuthBar from "./AuthBar";
 export default function Navbar() {
 	return (
 		<nav className="fixed top-0 left-0 right-0 bg-white border-b py-3 z-50">
-			<div className="flex justify-between max-w-7xl mx-auto px-5 items-center">
+			<div className="flex justify-between max-w-3xl mx-auto px-5 items-center">
 				<Link className="font-bold text-2xl" href={"/"}>
 					PostBook
 				</Link>
