1ca2d034f947dc3ee60245ccc9b72e3f68c17251
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-18 18:10:07 +0600

    update single post page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import PostCard from "@/components/post/PostCard";
+import PostContext from "@/contexts/PostContext";
+import { useParams } from "next/navigation";
+import { useContext, useState } from "react";
+
 export default function page() {
-  return <div>Single post page</div>;
+  const {id} = useParams();
+  const {getPostById} = useContext(PostContext);
+  const post = getPostById(Number(id));
+  const [isEdit, setIsEdit] = useState(false);
+
+	return (
+		<div className="max-w-3xl mx-auto px-5 sm:px-8 md:px-10 py-5">
+			{post && <PostCard {...post} singleComment={false} />}
+		</div>
+	);
 }
