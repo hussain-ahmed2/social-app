f68168e483504324d7f1f21bfe044c9fc0358bb6
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-18 18:11:38 +0600

    update post card add single and multiple comment option for single post page and all posts page

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,7 +1,12 @@
 import { Post } from "@/types";
 import PostAuthor from "./PostAuthor";
 import LikeBtn from "./LikeBtn";
-import Link from "next/link";
+import CommentBtn from "./CommentBtn";
+import DisplayComments from "./DisplayComments";
+
+interface PostCardProps extends Post {
+	singleComment?: boolean;
+}
 
 export default function PostCard({
 	content,
@@ -9,8 +14,9 @@ export default function PostCard({
 	authorId,
 	updatedAt,
 	id,
-	imageUrl
-}: Post) {
+	imageUrl,
+	singleComment = true,
+}: PostCardProps) {
 	return (
 		<article className="border p-5 rounded-lg shadow-sm bg-neutral-100">
 			<div className="mb-4">
@@ -22,17 +28,26 @@ export default function PostCard({
 						{title}
 					</h2>
 					<p className="text-neutral-800 mt-2">{content}</p>
-					{
-						imageUrl && <img className="w-full mt-2" src={imageUrl} alt={`post-image-${id}`} />
-					}
+					{imageUrl && (
+						<img
+							className="w-full mt-2"
+							src={imageUrl}
+							alt={`post-image-${id}`}
+						/>
+					)}
 					<p className="text-neutral-500 text-[0.7rem] mt-2 text-right">
 						{updatedAt}
 					</p>
 				</div>
 			</div>
-			<div className="mt-2 flex items-center justify-between">
-				<LikeBtn postId={id} />
-				<Link className="text-sky-500 font-light text-sm hover:underline" href={`/posts/${id}`}>Read more</Link>
+			<div className="mt-2 flex items-start flex-wrap">
+				<div className="w-1/2 flex items-center gap-3">
+					Like <LikeBtn postId={id} />
+				</div>
+				<CommentBtn postId={id} />
+			</div>
+			<div>
+				<DisplayComments singleComment={singleComment} postId={id} />
 			</div>
 		</article>
 	);
