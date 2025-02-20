9b360f7f235897ca3e0f7e478a177420932c7264
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-20 09:23:48 +0600

    update all post page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { useContext } from "react";
+import PostContext from "@/contexts/PostContext";
+import PostCard from "@/components/post/PostCard";
+
 export default function page() {
-  return <div>Posts page</div>;
+	const { getPosts } = useContext(PostContext);
+	const posts = getPosts();
+	return (
+		<div className="max-w-3xl mx-auto px-5 sm:px-8 md:px-10">
+			<h1 className="text-center font-bold text-2xl my-5">All Posts</h1>
+			<div className="flex flex-col gap-5 mb-5">
+				{posts &&
+					posts.map((post) => (
+						<PostCard
+							key={post.id}
+							{...post}
+							singleComment={true}
+						/>
+					))}
+			</div>
+		</div>
+	);
 }
