e306a8f880c70adec8053a640c8bb928a54c3ffc
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-20 09:22:05 +0600

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import PostTextarea from "@/components/home/PostTextarea";
 import PostCard from "@/components/post/PostCard";
 import AuthContext from "@/contexts/AuthContext";
 import PostContext from "@/contexts/PostContext";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { useContext, useEffect } from "react";
 
@@ -20,9 +21,18 @@ export default function Home() {
 				<PostTextarea />
 			</div>
 			<div className="flex flex-col gap-5 mb-5">
-				{
-					posts && posts.map(post => <PostCard key={post.id} {...post} />)
-				}
+				{posts &&
+					posts
+						.slice(0, 10)
+						.map((post) => <PostCard key={post.id} {...post} />)}
+			</div>
+			<div>
+				<Link
+					href="/posts"
+					className="text-neutral-950 transition mb-5 px-6 py-2 bg-neutral-300 rounded-md block w-fit mx-auto hover:bg-neutral-400 hover:shadow-md"
+				>
+					View all posts
+				</Link>
 			</div>
 		</div>
 	);
