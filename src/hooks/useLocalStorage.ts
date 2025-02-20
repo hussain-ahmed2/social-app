54393905203710526461f221c5456ad20eb5d5b3
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-20 09:41:16 +0600

    update use localstorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,14 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function useLocalStorage<T>(key: string, initialValue: T) {
-	const storedValue = localStorage.getItem(key);
-	const [data, setData] = useState<T>(
-		storedValue ? JSON.parse(storedValue) : initialValue
-	);
+function useLocalStorage<T>(key: string, initialValue: T) {
+	const [value, setValue] = useState<T>(initialValue);
+	const [isClient, setIsClient] = useState(false);
 
 	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(data));
-	}, [key, data]);
+		setIsClient(true);
+		try {
+			const storedValue = localStorage.getItem(key);
+			if (storedValue !== null) {
+				setValue(JSON.parse(storedValue));
+			}
+		} catch (error) {
+			console.error("Error reading localStorage key:", key, error);
+		}
+	}, [key]);
 
-	return [data, setData] as const;
+	useEffect(() => {
+		if (isClient) {
+			try {
+				localStorage.setItem(key, JSON.stringify(value));
+			} catch (error) {
+				console.error("Error setting localStorage key:", key, error);
+			}
+		}
+	}, [isClient, key, value]);
+
+	return [value, setValue] as const;
 }
+
+export default useLocalStorage;
