85f376b9bfddd2949a602b0d7d7bca84a182fb36
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-20 09:22:45 +0600

    add title

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -9,10 +9,10 @@ export default function page() {
   const {id} = useParams();
   const {getPostById} = useContext(PostContext);
   const post = getPostById(Number(id));
-  const [isEdit, setIsEdit] = useState(false);
 
 	return (
 		<div className="max-w-3xl mx-auto px-5 sm:px-8 md:px-10 py-5">
+			<h1 className="text-center font-bold text-2xl my-5">Post</h1>
 			{post && <PostCard {...post} singleComment={false} />}
 		</div>
 	);
